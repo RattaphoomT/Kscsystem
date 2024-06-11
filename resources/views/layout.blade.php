b6531ede89420e9378a508f0206968fc9355b240
RattaphoomT <rattaphoom.t@kkumail.com>
2024-06-11 17:00:23 +0700

    comit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -834,10 +834,24 @@
 
             });
         });
-
     </script>
 
 
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/66681f31981b6c56477be053/1i03bjmkf';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 
 
     @yield('scripts')
