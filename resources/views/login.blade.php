48049aed2b869f6a1fd31be88e8879959113ae6f
RattaphoomT <rattaphoom.t@kkumail.com>
2024-06-12 16:44:27 +0700

    comit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -38,7 +38,7 @@
             left: 0;
             width: 100%;
             height: 100%;
-            background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ asset('img/bg.jpg') }}');
+            background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('{{ asset('img/swim.jpg') }}');
             background-size: cover;
             background-position: center;
         }
